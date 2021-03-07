bb47d9dca9a44d66a8fc9703cf8541f1c7db7e4b
giegeralexandra <giegeralexandra@gmail.com>
2021-03-07 10:18:20 -0500

    commit tree 0a0effaeb060926b419e41d084f4f62d0e487d55

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      if (Array.isArray(collection)){
+        let newCollection = []
+        newCollection.push(collection.forEach( c => {callback(c)}))
+      } else {
+        let newCollection = {}
+        newCollection.push(collection.forEach( c => {callback(c)}))
+      }
+      return collection
     },
 
-    map: function() {
+    map: function(collection, callback) {
 
     },
 
